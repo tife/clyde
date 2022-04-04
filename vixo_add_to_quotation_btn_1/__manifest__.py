a45f7d68623382e36c33b88718450f21d50a1fda
im-root <aksharzalavadiya143@gmail.com>
2022-04-04 13:31:21 +0530

    [ADD] Task 1 Added

---
+++ vixo_add_to_quotation_btn_1/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Clyde Custom',
+    'category': 'Website/eCommerce',
+    'summary': 'clyde custom.',
+    'description': 'clyde custom.',
+    'version': '15.0.1.0.1',
+    'author': 'Akshar Zalavadiya',
+    'data': [
+        'views/product_template.xml',
+    ],
+    'depends': [
+        'website_sale',
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+}
