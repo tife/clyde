a885c9fbbe1af6f4b9aa1a8a3ef48acd905310a1
Tife Chan <tife.chan@vicosys.com.hk>
2022-04-04 17:55:19 +1000

    Product image hover, show secondary image

---
+++ vixo_secondary_product_image/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Secondary Product Image Hover',
+    'summary': 'Secondary Product Image Hover',
+    'description': 'When mouse of the product image, display the secondary proudct image. Add a product list component with "Borderless Product n*1" template. In the product carousel container, add a class "hover-sec-image" to enable this feature',
+    'version': '1.0',
+    'author': 'VIXO Digital Limited',
+    'category': 'Website',
+
+    'depends': ['website'],
+    'data': [
+        'template.xml',
+    ],
+}
