8fb1f14e790d042f3d772a3ef2bee8ed73db93a7
nirav11388 <niravparmar@gmail.com>
2022-06-21 16:13:36 +0530

    [FIX] Issue Fixed

--- vixo_add_to_quotation/models/product_template.py
+++ vixo_add_to_quotation/models/product_template.py
@@ -9,7 +9,7 @@ class ProductTemplate(models.Model):
 
     product_banner_image = fields.Binary(string='Product Banner Image(Top)')
     iframe_url = fields.Char('3D Image Url')
-    specification = fields.Html()
+    # specification = fields.Html()
     product_details_image = fields.Binary(string='Product Detail Image(Bottom)')
     product_attachment = fields.Binary(string='Specification Sheet')
     product_attachment_dwg = fields.Binary(string='DWG')
